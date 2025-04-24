9aa3f9d87175b9bef7823e0a2b256eff5e34577e
Ananya54321 <ananyapappula521@gmail.com>
2025-04-24 13:07:20 +0530

    added nfts page

---
+++ src/app/nfts/page.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import TrendingNfts from "@/components/pages/nfts/TrendingNfts";
+import NewestNfts from "@/components/pages/nfts/NewestNfts";
+
+const page = () => {
+  return (
+    <div>
+      {/* trending nfts */}
+      <NewestNfts />
+      <TrendingNfts />
+      {/* trending nfts */}
+      {/* top nfts */}
+      {/* top nfts */}
+      {/* newest nfts */}
+      {/* newest nfts */}
+    </div>
+  );
+};
+
+export default page;
