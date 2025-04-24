b03b6e6a650bd5ffd83cca4ffa2b6c536851e592
YadlaMani <yadlamaniymn2005@gmail.com>
2025-04-24 13:20:17 +0530

    market hooks

---
+++ src/hooks/useFetchMarket.ts
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+export default function useFetchMarketData() {
+  const [data, setData] = useState([]);
+  const [err, setErr] = useState("");
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const res = await axios.get(
+          "https://pro-api.solscan.io/v2.0/market/list?page=1&page_size=10&sort_by=created_time&sort_order=desc",
+          {
+            headers: {
+              token: process.env.NEXT_PUBLIC_API_KEY,
+            },
+          }
+        );
+        setData(res.data);
+      } catch (error) {
+        setErr("failed to fetch data");
+        console.error("Error fetching market data:", error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  return { data, err };
+}
