a564b827eec6d1cec391b0bf7da1bdd8da294834
YadlaMani <yadlamaniymn2005@gmail.com>
2025-04-26 14:52:47 +0530

    navbar and metadata done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/ui/theme-provider";
+import { Appbar } from "@/components/AppBar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,7 +35,10 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <div>{children}</div>
+          <div>
+            <Appbar />
+            {children}
+          </div>
         </ThemeProvider>
       </body>
     </html>
